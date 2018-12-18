7005b0ce8b6fc5891c070677a023ff5cb7a77b03
ahmadragheb <ahmedragheb75@gmail.com>
2018-12-18 03:30:06 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+## Merias
+
+simple custoization to merias co
+
+#### License
+
+MIT
---
+++ merias/config/docs.py
@@ -0,0 +1,11 @@
+"""
+Configuration for docs
+"""
+
+# source_link = "https://github.com/[org_name]/merias"
+# docs_base_url = "https://[org_name].github.io/merias"
+# headline = "App that does everything"
+# sub_heading = "Yes, you got that right the first time, everything"
+
+def get_context(context):
+	context.brand_html = "Merias"
