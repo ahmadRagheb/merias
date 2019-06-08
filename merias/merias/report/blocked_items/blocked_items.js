dac973c74409757b7c9ec81fae8661f499ea85c8
ahmadragheb <ahmedragheb75@gmail.com>
2019-06-08 23:16:14 +0300

    blocked report

---
+++ merias/merias/report/blocked_items/blocked_items.js
@@ -0,0 +1,9 @@
+// Copyright (c) 2016, ahmadragheb and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+
+frappe.query_reports["Blocked Items"] = {
+	"filters": [
+
+	]
+}
---
+++ merias/merias/report/blocked_items/blocked_items.py
@@ -0,0 +1,69 @@
+# Copyright (c) 2013, ahmadragheb and contributors
+# For license information, please see license.txt
+
+from __future__ import unicode_literals
+import frappe
+from frappe import _
+from frappe.utils import flt
+
+
+def execute(filters=None):
+	if not filters: filters = {}
+
+	columns = get_columns()
+	data = get_data(filters)
+
+	return columns, data
+
+
+def get_columns():
+	return [
+		_("Sales Order") + ":Link/Sales Order:120",
+		_("Item") + ":Link/Item:120",
+		_("qty") + "::60"
+	]
+
+def get_data(filters):
+	conditions = get_conditions(filters)
+
+	dn = frappe.db.sql("""select against_sales_order , item_code, sum(qty)
+	from `tabDelivery Note Item` where docstatus =1 and against_sales_order IS NOT NULL
+	group by against_sales_order""", as_dict=1)
+
+	dn_dict = {}
+	for item in dn:
+	   against_sales_order = item.pop('against_sales_order')
+	   dn_dict[against_sales_order] = item
+
+	so = frappe.db.sql("""SELECT so.name, smi.item_code, smi.qty FROM `tabSales Order` so
+	LEFT JOIN `tabSales Order Item` smi ON  so.name = smi.parent and  smi.is_blocked = 1
+	and so.status not in ('Cancelled','Completed', 'Draft', 'Closed')
+	 where smi.qty IS NOT NULL""", as_dict=1)
+	so_dict = {}
+	for item in so:
+	   # name = item.pop('name')
+	   name = item['name']
+	   so_dict[name] = item
+
+	for so in so_dict :
+		if so in dn_dict:
+			qty = dn_dict[so].qty
+			print(qty)
+			so_dict[so].qty = flt(so_dict[so].qty) - flt(qty)
+
+	result = [x.values() for x in so_dict.values()]
+
+	return result
+
+
+def get_conditions(filters):
+	conditions = ""
+	if filters.get("month"):
+		month = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov",
+			"Dec"].index(filters["month"]) + 1
+		conditions += " and month(date_of_birth) = '%s'" % month
+
+	if filters.get("company"): conditions += " and company = '%s'" % \
+		filters["company"].replace("'", "\\'")
+
+	return conditions
