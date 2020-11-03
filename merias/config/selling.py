0fe884708e1e15f52cd5d65ca7f482c755e5ae9f
zaqoutabed <zaqoutabed@gmail.com>
2020-11-03 18:05:51 +0200

    add Blocked Items to Selling Board

---
+++ merias/config/selling.py
@@ -0,0 +1,26 @@
+from __future__ import unicode_literals
+from frappe import _
+
+
+def get_data():
+    return [
+        {
+            "label": _("Blocked Items"),
+            "icon": "fa fa-list",
+            "items": [
+                {
+                    "type": "doctype",
+                    "name": "Release Blocked Item",
+                    "description": _("Release Blocked Item."),
+                    "onboard": 1,
+                },
+                {
+                    "type": "report",
+                    "is_query_report": True,
+                    "name": "Blocked Items",
+                    "doctype": "Item"
+                }
+            ]
+        },
+
+    ]
